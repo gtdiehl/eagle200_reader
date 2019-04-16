1fa3c2ee04c0660955bbf93b727f8b5167a1d2a5
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-04-15 17:12:24 -0700

    Updated eagle200_reader.py
    Added error handling for network connection issues.  Added comments and TODO messages.  Added a new function summation_total that gives one value for the summation delivered minus received.

--- eagle200_reader/eagle200_reader.py
+++ eagle200_reader/eagle200_reader.py
@@ -1,5 +1,5 @@
 import requests
-import sys, traceback
+import sys
 import xml.etree.ElementTree as ET
 
 HTTP_DEVICE_LIST = """
@@ -8,35 +8,112 @@ HTTP_DEVICE_LIST = """
     </Command>
 """
 
+'''
+TODO:
+    - Add logging messages for better debugging
+'''
+
 class EagleReader:
 
     def __init__(self, ip_addr, cloud_id, install_code):
         self.ip_addr = ip_addr
         self.cloud_id = cloud_id
         self.install_code = install_code
+    
+    '''
+    Entry point from public functions
+    '''    
+    def _call_api(self):
+        '''
+        Get the hardware address of the Power Meter.  This address is required
+        to build the XML Post request.
+        '''
+        devices = self._get_device_address()
         
-    def call_api(self):
-        devices = self.get_device_address()
+        '''
+        Currently only one Power Meter and no other devices associated with
+        the Eagle-200 is supported.  Check if the length of the list is 1, > 1,
+        or none.
+        
+        An empty devices list can occur if a timeout occurs while making the 
+        HTTP Post request to get the hardware address.
+        '''
         if len(devices) == 1:
-            http_device_query = self.build_xml_device_query(devices[0])
-            response = requests.post("http://" + self.ip_addr + 
-                "/cgi-bin/post_manager", http_device_query, auth=(self.cloud_id, self.install_code))
+            http_device_query = self._build_xml_device_query(devices[0])
+            try:
+                response = requests.post("http://" + self.ip_addr + 
+                    "/cgi-bin/post_manager", http_device_query, auth=(self.cloud_id, self.install_code), timeout=2)
+                '''
+                This exception occurs after if a resonse from the request is not received
+                in 2 seconds. And returns None
+                '''
+            except requests.exceptions.Timeout as e:
+                return None
+                '''
+                This exception occurs if a new connection cannot be made to the
+                Eagle-200. And returns None
+                '''
+            except requests.exceptions.ConnectionError as e:
+                return None
+                '''
+                Returns None for all other Request Exceptions
+                '''
+            except requests.exceptions.RequestException as e:
+                return None
+                '''
+                If no exceptions occur, process the response and create a dictionary
+                of all variable names and values
+                '''
+            else:
+                return self._create_attributes(response)
         elif len(devices) > 1 and len(devices) != 0:
             print("Currently only the API only supports a single device")
+            return None
         else:
-            print("Device list is empty!")            
-        return self.create_attributes(response)
-
-    def get_device_address(self):
+            print("Device list is empty!")
+            return None
+        
+    '''
+    This function gets the hardware address of the Power Meter than it associated
+    to the Eagle-200 device.
+    
+    TODO: 
+        - Update function to handle multiple Power Meters
+        - Update function to handle other types of devices
+          that can be associated to the Eagle-200; ie: Thermostat or SmartPlug
+    '''
+    def _get_device_address(self):
         devices = []
-        response = requests.post("http://" + self.ip_addr + 
-            "/cgi-bin/post_manager", HTTP_DEVICE_LIST, auth=(self.cloud_id, self.install_code))
-        tree = ET.fromstring(response.content)
-        for node in tree.iter('HardwareAddress'):
-            devices.append(node.text)
-        return devices
+        try:
+            response = requests.post("http://" + self.ip_addr + 
+                "/cgi-bin/post_manager", HTTP_DEVICE_LIST, auth=(self.cloud_id, self.install_code), timeout=2)
+            '''
+            This exception occurs after if a resonse from the request is not received
+            in 2 seconds. And return an empty devices list
+            '''
+        except requests.exceptions.Timeout as e:
+            return devices
+            '''
+            This exception occurs if a new connection cannot be made to the
+            Eagle-200. And return an empty devices list
+            '''
+        except requests.exceptions.ConnectionError as e:
+            return devices
+            '''
+            Returns an empty devices list for all other Request Exceptions
+            '''
+        except requests.exceptions.RequestException as e:
+            return devices
+            '''
+            Process the XML if no exceptions occur
+            '''
+        else:
+            tree = ET.fromstring(response.content)
+            for node in tree.iter('HardwareAddress'):
+                devices.append(node.text)
+            return devices
 
-    def build_xml_device_query(self, hardware_address):
+    def _build_xml_device_query(self, hardware_address):
         http_device_query = """
             <Command>
               <Name>device_query</Name>
@@ -50,7 +127,14 @@ class EagleReader:
         """
         return http_device_query
 
-    def create_attributes(self, xml_output):
+    '''
+    Builds a dictionary of all the Variable names and values of the Attributes
+    that are returned from the Power Meter that is associated to the Eagle-200
+    
+    TODO:
+        - Need to handle the case when building the dictionary fails
+    '''
+    def _create_attributes(self, xml_output):
         i = 0
         attribs = []
         tree = ET.fromstring(xml_output.content)
@@ -61,28 +145,39 @@ class EagleReader:
             i = i + 1
         return attribs
 
-    def get_value(self, attribs, value_name):
+    '''
+    Searches the dictionary created by the create_attributes() function for a
+    specific Variable name and returns its value.
+    
+    TODO:
+        - Need to handle the case when a value is not found in the dictionary
+    '''
+    def _get_value(self, attribs, value_name):
         for child in attribs:
             if child[0]['Name'] == value_name:
                 return child[1]['Value']
     
     def instantanous_demand(self):
         try:
-            device_attributes = EagleReader.call_api(self)
-            instantanous_demand = self.get_value(device_attributes, 'zigbee:InstantaneousDemand')
+            device_attributes = EagleReader._call_api(self)
+            if device_attributes is None:
+                return None
+            instantanous_demand = self._get_value(device_attributes, 'zigbee:InstantaneousDemand')
             # Eagle-200 has a bug, sometimes the Value for Instantanous Demand will be blank.  When this
             # occurs a value of None will be returned
             if instantanous_demand is not None:
                 return float(instantanous_demand)
             else:
                 return None
-        except Exception:
-            traceback.print_exc(file=sys.stdout)
+        except Exception as e:
+            print(e)
             
     def summation_delivered(self):
         try:
-            device_attributes = EagleReader.call_api(self)
-            summation_delivered = self.get_value(device_attributes, 'zigbee:CurrentSummationDelivered')
+            device_attributes = EagleReader._call_api(self)
+            if device_attributes is None:
+                return None
+            summation_delivered = self._get_value(device_attributes, 'zigbee:CurrentSummationDelivered')
             # Eagle-200 has a bug, sometimes the Value for Summation Delivered will have the incorrect
             # decimal place.  If six zeros to the right of the decimal point are found than it'll assume
             # the value is incorrect and return None
@@ -90,14 +185,16 @@ class EagleReader:
                 return float(summation_delivered)
             else:
                 return None
-        except Exception:
-            traceback.print_exc(file=sys.stdout)
+        except Exception as e:
+            print(e)
 
     
     def summation_received(self):
         try:
-            device_attributes = EagleReader.call_api(self)
-            summation_received = self.get_value(device_attributes, 'zigbee:CurrentSummationReceived')
+            device_attributes = EagleReader._call_api(self)
+            if device_attributes is None:
+                return None
+            summation_received = self._get_value(device_attributes, 'zigbee:CurrentSummationReceived')
             # Eagle-200 has a bug, sometimes the Value for Summation Received will have the incorrect
             # decimal place.  If six zeros to the right of the decimal point are found than it'll assume
             # the value is incorrect and return None
@@ -105,11 +202,21 @@ class EagleReader:
                 return float(summation_received)
             else:
                 return None
-        except Exception:
-            traceback.print_exc(file=sys.stdout)
+        except Exception as e:
+            print(e)
+            
+    def summation_total(self):
+        delivered = self.summation_delivered()
+        received = self.summation_received()
+        
+        if delivered is not None and received is not None:
+            return delivered - received
+        else:
+            return None
 
 if __name__ == "__main__":
     testreader = EagleReader(sys.argv[1], sys.argv[2], sys.argv[3])
     print("Instantanous Demand:     {} kW".format(testreader.instantanous_demand()))
     print("Total Energy Delivered:  {} kWh".format(testreader.summation_delivered()))
     print("Total Energy Received:   {} kWh".format(testreader.summation_received()))
+    print("Total Net Energy:        {} kWh".format(testreader.summation_total()))
