909c573b9c940e0f79929eef989c9af1f175d942
Greg <34967045+gtdiehl@users.noreply.github.com>
2020-03-10 19:16:27 -0700

    Update eagle200_reader.py
    Fixed typo

--- eagle200_reader/eagle200_reader.py
+++ eagle200_reader/eagle200_reader.py
@@ -200,7 +200,7 @@ class EagleReader:
             return None
 
     def _summation_total(self, device_attributes):
-        if self.summation_delivered_value is None 
+        if self.summation_delivered_value is None:
             self.summation_delivered_value = 0.0
 
         if self.summation_received_value is None:
